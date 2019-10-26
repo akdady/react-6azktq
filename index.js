d3728ab08f304bc93ea092b46037962d114ba1aa
akdady <akdady@gmail.com>
2019-10-26 23:40:09 +0300

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
+import nav from './nav';
 import './style.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'Kayhan'
     };
+  }
+  render() {
+    return (
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt="kayhan"
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />
+      {' React Bootstrap'}
+    </Navbar.Brand>
+  </Navbar> );
   }
 
   render() {
     return (
-      <div>
+        <div>
         <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
---
+++ nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+<Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />
+      {' React Bootstrap'}
+    </Navbar.Brand>
+  </Navbar>
